3d8a1d98cd7fea02449adf8b1c140d8f592e3f0b
Eliu Gastelum <egast@gmail.com>
2023-12-05 14:02:34 -0700

    Modificacion main, Eliminacion clases innecesarias

--- Main.java
+++ Main.java
@@ -1,32 +1,76 @@
-
 import java.util.Scanner;
 
 public class Main {
-	static Scanner entrada = new Scanner(System.in);
+	
     public static void main(String[] args) {
-        System.out.println("----------------------------------------------------");
-        System.out.println("Ingrese su Nombre:	(Catalina)");
-        String Nombre = entrada.nextLine();
-        System.out.println("Ingrese su Numero de cuenta:	(23140972)");
-        String Num_cuenta = entrada.nextLine();
-        System.out.println("Ingrese su Nip:		(8002)");
-        int Nip = entrada.nextInt();
-        System.out.println("----------------------------------------------------");
-        if(Nombre.equals("Catalina") && Num_cuenta.equals("23140972") && Nip==8002) {
-        	System.out.println("===============================================");
-        	System.out.print(Nombre);
-            Clase_Abstractaa mesajero = new Consulta();
-            mesajero.setSaldo(1700);
-            mesajero.Operaciones();
-        }else {
-        	System.out.println("===============================================");
-            System.out.println("Alguno de sus datos es erroneo, intente denuevo");
-        	System.out.println("===============================================");
-   
-        }
+        iniciar();
     }
-    public String getNombre(){
-        return getNombre();
+
+    public static void iniciar(){
+        Scanner scan = new Scanner(System.in);
+        Transaccion tran = null;
+        Cajero cajero1 = new Cajero("Soriana", "BBVA");
+        Cliente cliente1 = new Cliente("1111", "Gilberto Maldonado", "Enrique Segoviano");
+        Cuenta cuentaAhorros = new Cuenta(1,cliente1,"11101101", 20000.00);
+        TarjetaDebito tj1 = new TarjetaDebito(cuentaAhorros,cliente1);
+        String aux;
+
+        Cliente cliente2 = new Cliente("2222", "Eloi", "amlo");
+        Cuenta cuentaCheques = new Cuenta(2,cliente2,"10101101", 2000.00);
+        TarjetaDebito tj2 = new TarjetaDebito(cuentaCheques,cliente2);
+
+
+        int op;
+        String opcion;
+        do {
+            System.out.println("Bienvenido al cajero\n\n");
+            System.out.println("Ingrese Tarjeta\n");
+            System.out.println("Se ingreso tarjeta tj1\n");
+            System.out.println("Cliente: " + tj1.getCliente().getNombre());
+            aux = cuentaAhorros.getTipo() == 1 ? "Ahorros" : "Cheques";
+            System.out.println("Cuenta: " + aux);
+            System.out.println("Saldo: " + tj1.getCuenta().getSaldo());
+            System.out.println("\nOperaciones a realizar:");
+            System.out.println("1.Retiro");
+            System.out.println("2.Transferir");
+            System.out.println("3.Salir");
+            System.out.println("Ingrese una opcion:");
+            op = scan.nextInt();
+            
+
+            switch (op) {
+                case 1:
+                    //retirar();
+                    tran = cajero1.retirar(tj1.getCuenta(), 1, ingresarmonto());
+                    break;
+                case 2:
+                    //transferir()
+                    tran = cajero1.transferir(tj1.getCuenta(), tj2.getCuenta(), 2, ingresarmonto());
+                    break;
+                case 3:
+                    System.out.println("Saliendo del cajero. ¡Hasta luego!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, ingrese una opción válida.");
+                    break;
+            }
+
+            if (tran != null) {
+                cajero1.imprimirTicket(cliente1, tran);
+            } else {
+                System.out.println("No se pudo realizar la transacción");
+            }
+
+            System.out.println("\n¿Desea realizar otra operación? (Sí/No): ");
+            opcion = scan.next().toLowerCase();
+        } while (opcion.equals("si"));
+    }
+    public static double ingresarmonto(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese monto a retirar");
+        double monto = scan.nextDouble();
+        return monto;
     }
 }
 
